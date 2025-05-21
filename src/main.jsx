a2aec947a446633559122f46f7a25d11860f6c2a
Kimbowa Owen <owenmicheal3rd@gmail.com>
2025-05-21 13:12:05 +0300

    implements rel icon

--- src/main.jsx
+++ src/main.jsx
@@ -1,22 +1,21 @@
-import React from "react";
 import ReactDOM from "react-dom/client"; // Import from 'react-dom/client' in React 18
 import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
 
 import App from "./components/App";
 import store from "./app/store";
-import './index.css';
+import "./index.css";
 import TogleColorMode from "./utils/TogleColorMode";
 
-
 // Create a root element using createRoot and render your App
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-    <Provider store={store}>
+  <Provider store={store}>
     <TogleColorMode>
-    <BrowserRouter>
-         <App />
-    </BrowserRouter>
-    </TogleColorMode>,
-    </Provider>
-    );
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </TogleColorMode>
+    ,
+  </Provider>
+);
